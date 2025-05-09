ec7e6d27b9bd0229c33fc7616309a4fb1a7ba2f6
ayawata428 <ayaka.lovefen@gmail.com>
2025-05-09 15:13:00 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,12 @@
+
+const textElement = document.getElementById('text');
+const btnElement = document.getElementById('btn');
+
+console.log(textElement);
+console.log(btnElement);
+
+btnElement.addEventListener('click',() => {
+  setTimeout(() => {
+    textElement.textContent = 'ボタンをクリックしました';
+  }, 2000);
+});
